9db9496e52e5a33166ff3118da9aaf684f032172
James Coglan <jcoglan@gmail.com>
2019-06-05 18:02:51 +0100

    Parse committer information from commit objects

--- src/objects/commit.rs
+++ src/objects/commit.rs
@@ -1,18 +1,24 @@
 use std::collections::HashMap;
-use std::io::BufRead;
+use std::error::Error;
+use std::io::{BufRead, Read};
+use std::str::FromStr;
 
 use super::Id;
 
 #[derive(Default)]
 pub struct Commit {
     headers: HashMap<String, Vec<String>>,
+    committer: Option<Author>,
     message: String,
 }
 
 impl Commit {
     pub fn parents(&self) -> impl Iterator<Item = Id> + '_ {
-        let ids = self.headers.get("parent").into_iter().flatten();
-        ids.map(|id| Id::from(id.as_str()))
+        self.header("parent").map(|id| Id::from(id.as_str()))
+    }
+
+    fn header(&self, key: &str) -> impl Iterator<Item = &String> {
+        self.headers.get(key).into_iter().flatten()
     }
 }
 
@@ -24,6 +30,12 @@ impl<R: BufRead> From<R> for Commit {
             commit.headers.entry(key).or_default().push(value);
         }
 
+        let committer = commit
+            .header("committer")
+            .next()
+            .and_then(|s| s.parse().ok());
+
+        commit.committer = committer;
         reader.read_to_string(&mut commit.message).ok();
         commit
     }
@@ -48,4 +60,35 @@ impl<R: BufRead> Iterator for Headers<'_, R> {
 
         Some((key, value))
     }
+}
+
+struct Author {
+    time: time::Tm,
+}
+
+impl FromStr for Author {
+    type Err = Box<dyn Error>;
+
+    fn from_str(string: &str) -> Result<Self, Self::Err> {
+        let mut bytes = string.as_bytes();
+
+        let _name = read_until(&mut bytes, b'<')?;
+        let _email = read_until(&mut bytes, b'>')?;
+
+        let mut time_str = String::new();
+        bytes.read_to_string(&mut time_str)?;
+        let time = time::strptime(time_str.trim(), "%s %z")?;
+
+        Ok(Author { time })
+    }
+}
+
+fn read_until(bytes: &mut &[u8], sep: u8) -> Result<String, Box<dyn Error>> {
+    let mut vec = Vec::new();
+    bytes.read_until(sep, &mut vec)?;
+
+    let mut string = String::from_utf8(vec)?;
+    string.truncate(string.len() - 1);
+
+    Ok(string.trim().to_string())
 }
