5d226af27bdb2274061ad33df7851a77d83dcd71
James Coglan <jcoglan@gmail.com>
2019-06-05 15:57:22 +0100

    Use an Iterator to parse commit headers

--- src/objects/commit.rs
+++ src/objects/commit.rs
@@ -20,12 +20,8 @@ impl<R: BufRead> From<R> for Commit {
     fn from(mut reader: R) -> Self {
         let mut commit = Commit::default();
 
-        loop {
-            if let Some((key, value)) = read_header(&mut reader) {
-                commit.headers.entry(key).or_default().push(value);
-            } else {
-                break;
-            }
+        for (key, value) in Headers(&mut reader) {
+            commit.headers.entry(key).or_default().push(value);
         }
 
         reader.read_to_string(&mut commit.message).ok();
@@ -33,17 +29,23 @@ impl<R: BufRead> From<R> for Commit {
     }
 }
 
-fn read_header<R: BufRead>(reader: &mut R) -> Option<(String, String)> {
-    let mut line = String::new();
-    reader.read_line(&mut line).ok()?;
+struct Headers<'a, R>(&'a mut R);
 
-    if line.trim().is_empty() {
-        return None;
+impl<R: BufRead> Iterator for Headers<'_, R> {
+    type Item = (String, String);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut line = String::new();
+        self.0.read_line(&mut line).ok()?;
+
+        if line.trim().is_empty() {
+            return None;
+        }
+
+        let mut parts = line.splitn(2, ' ').map(|s| s.trim().to_string());
+        let key = parts.next()?;
+        let value = parts.next()?;
+
+        Some((key, value))
     }
-
-    let mut parts = line.splitn(2, ' ').map(|s| s.trim().to_string());
-    let key = parts.next()?;
-    let value = parts.next()?;
-
-    Some((key, value))
 }
