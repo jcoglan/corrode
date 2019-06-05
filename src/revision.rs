21dd06b861c0a4be5ac72cfdc04236d2c55d8a69
James Coglan <jcoglan@gmail.com>
2019-06-05 16:25:34 +0100

    Introduce sub-structs into RevList to isolate readable and writable structures; this lets us make add_parents() a bit clearer

--- src/revision.rs
+++ src/revision.rs
@@ -1,25 +1,22 @@
 use std::collections::{HashMap, HashSet, VecDeque};
 
+use crate::db::Database;
 use crate::objects::{Commit, Id, WithId};
 use crate::repo::Repository;
 
 pub struct RevList<'a> {
     repo: &'a Repository,
-    flags: HashMap<Id, HashSet<Flag>>,
-    queue: VecDeque<WithId<Commit>>,
-}
-
-#[derive(PartialEq, Eq, Hash)]
-enum Flag {
-    Added,
-    Seen,
+    commits: Commits<'a>,
+    queue: Queue,
 }
 
 impl<'a> RevList<'a> {
     pub fn new(repo: &'a Repository, args: &[String]) -> Self {
-        let flags = HashMap::new();
-        let queue = VecDeque::new();
-        let mut rev_list = RevList { repo, flags, queue };
+        let mut rev_list = RevList {
+            repo,
+            commits: Commits(&repo.database),
+            queue: Queue::default(),
+        };
 
         for rev in args {
             rev_list.set_start_point(rev);
@@ -30,36 +27,24 @@ impl<'a> RevList<'a> {
 
     fn set_start_point(&mut self, rev: &str) {
         let id = self.repo.refs.read_ref(rev);
-        let opt_commit = id.and_then(|id| self.load_commit(&id));
+        let opt_commit = id.and_then(|id| self.commits.load(&id));
 
         if let Some(commit) = opt_commit {
-            self.enqueue_commit(commit);
-        }
-    }
-
-    fn enqueue_commit(&mut self, commit: WithId<Commit>) {
-        if self.mark(&commit.id, Flag::Seen) {
-            self.queue.push_back(commit);
+            self.queue.push(commit);
         }
     }
 
     fn add_parents(&mut self, commit: &WithId<Commit>) {
-        if !self.mark(&commit.id, Flag::Added) {
+        if !self.queue.mark(&commit.id, Flag::Added) {
             return;
         }
 
-        for id in commit.parents() {
-            self.load_commit(&id)
-                .map(|commit| self.enqueue_commit(commit));
+        let commits = &self.commits;
+        let parents = commit.parents().filter_map(|id| commits.load(&id));
+
+        for commit in parents {
+            self.queue.push(commit);
         }
-    }
-
-    fn load_commit(&self, id: &Id) -> Option<WithId<Commit>> {
-        self.repo.database.load(id).and_then(|obj| obj.as_commit())
-    }
-
-    fn mark(&mut self, id: &Id, flag: Flag) -> bool {
-        self.flags.entry(id.clone()).or_default().insert(flag)
     }
 }
 
@@ -67,9 +52,45 @@ impl<'a> Iterator for RevList<'a> {
     type Item = WithId<Commit>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let commit = self.queue.pop_front()?;
+        let commit = self.queue.pop()?;
         self.add_parents(&commit);
 
         Some(commit)
+    }
+}
+
+struct Commits<'a>(&'a Database);
+
+impl Commits<'_> {
+    fn load(&self, id: &Id) -> Option<WithId<Commit>> {
+        self.0.load(id).and_then(|obj| obj.as_commit())
+    }
+}
+
+#[derive(Default)]
+struct Queue {
+    flags: HashMap<Id, HashSet<Flag>>,
+    commits: VecDeque<WithId<Commit>>,
+}
+
+#[derive(PartialEq, Eq, Hash)]
+enum Flag {
+    Added,
+    Seen,
+}
+
+impl Queue {
+    fn push(&mut self, commit: WithId<Commit>) {
+        if self.mark(&commit.id, Flag::Seen) {
+            self.commits.push_back(commit);
+        }
+    }
+
+    fn pop(&mut self) -> Option<WithId<Commit>> {
+        self.commits.pop_front()
+    }
+
+    fn mark(&mut self, id: &Id, flag: Flag) -> bool {
+        self.flags.entry(id.clone()).or_default().insert(flag)
     }
 }
